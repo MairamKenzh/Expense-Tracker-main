256782b0f4b8cca36021b8b8e609c33d0511d9b5
MairamKenzh <mairam.kenzhebekkyzy@alatoo.edu.kg>
2024-12-14 11:10:27 +0600

    Delete Expense-Tracker-main — копия directory

--- Expense-Tracker-main — копия/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.expensetracker {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens org.example.expensetracker to javafx.fxml;
-    exports org.example.expensetracker;
-}
